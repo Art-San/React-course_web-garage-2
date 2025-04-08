4551ebdb34de26234920ab385f744b0ad7e16a8d
Art San <artsan.mcc@gmail.com>
2025-04-08 20:54:00 +0700

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ import { useUserContext } from '../contexts/UserContext'
 import UserInfo from './UserInfo'
 
 function Header() {
-  const user = useUserContext()
+  const { user, updateUser } = useUserContext()
   return (
     <div className=" flex items-center justify-center w-full h-30 bg-blue-200 gap-4">
-      <h1 className=" text-xl">Добро пожаловать, {user.name}!</h1>
+      <h1>Добро пожаловать, {user.name}!</h1>
+      <button
+        onClick={() => updateUser({ name: 'Вася', email: 'vasya@example.com' })}
+      >
+        Сменить пользователя
+      </button>
       <UserInfo />
     </div>
   )
