9cf7c46bd55af00d8ead603c78adbe1cc0f99884
Art San <artsan.mcc@gmail.com>
2025-04-11 22:40:14 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import AddUser from './components/AddUser'
-import DeleteUser from './components/DeleteUser'
-import UpdateUser from './components/UpdateUser'
-import UserList from './components/UserList'
+import { LoginForm } from './components/LoginForm'
+import { ProfileForm } from './components/ProfileForm'
+import { RegisterForm } from './components/RegisterForm'
+import useForm from './hooks/useForm'
 
 const App = () => {
+  const loginData = useForm({})
+  const registerData = useForm({})
+  const profileData = useForm({})
+
+  const handleSubmit = (e, formType) => {
+    e.preventDefault()
+    if (formType === 'login') {
+      console.log('Данные авторизации', loginData.formData)
+    } else if (formType === 'password') {
+      console.log('Данные регистрации', loginData.formData)
+    } else formType === ''
+    {
+      console.log('Иные данные', profileData.formData)
+    }
+  }
+
   return (
-    <div className=" flex flex-col h-screen items-center justify-center">
-      <div className="flex flex-col w-[300px] gap-4">
-        <h1 className=" text-4xl">Привет!</h1>
-        {/* <UserList /> */}
-        <AddUser />
-        <UpdateUser />
-        <DeleteUser />
-      </div>
+    <div className=" flex flex-col gap-2 items-center justify-center">
+      <h1 className="text-2xl font-bold">Авторизация</h1>
+      <LoginForm
+        {...loginData}
+        handleSubmit={(e) => handleSubmit(e, 'login')}
+      />
+      <h1 className="text-2xl font-bold">Регистрация</h1>
+      <RegisterForm
+        {...registerData}
+        handleSubmit={(e) => handleSubmit(e, 'password')}
+      />
+      <p>---------------</p>
+      <ProfileForm {...profileData} handleSubmit={(e) => handleSubmit(e, '')} />
     </div>
   )
 }
