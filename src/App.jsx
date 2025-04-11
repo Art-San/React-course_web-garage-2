02dfc93ff19b669e0c544ef5348afc676aea3cbe
Art San <artsan.mcc@gmail.com>
2025-04-11 23:44:48 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import MeasureElement from './components/MeasureElement'
+import { useRef } from 'react'
+import Input from './components/Input'
+import Page from './components/Page'
+import ParentComponent from './components/ParentComponent'
 
 const App = () => {
+  const inputRef = useRef(null)
+
+  const handleFocus = () => {
+    inputRef.current.fn1()
+  }
+
+  const handleGetValue = () => {
+    const value = inputRef.current.fn2()
+    console.log(value)
+  }
+
   return (
     <>
-      <MeasureElement />
+      <h1>Test</h1>
+      <div>
+        <Input ref={inputRef} />
+        <button onClick={handleFocus}>Фокус на input</button>
+        <button onClick={handleGetValue}>Получить значение</button>
+      </div>
+      <p>---------------------</p>
+      <Page />
+      <p>---------------------</p>
+      <ParentComponent />
     </>
   )
 }
