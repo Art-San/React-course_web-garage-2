7286503446c9aeb94fe81e52a417ce2c2ad4616e
Art San <artsan.mcc@gmail.com>
2025-04-15 12:06:19 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Counter from './components/Counter'
-import Todo from './components/Todo'
-
 const App = () => {
   return (
     <>
       <div className="flex  flex-col items-center mt-7 gap-10">
-        <Counter />
-        <Todo />
+        <h1>Топ</h1>
       </div>
     </>
   )
