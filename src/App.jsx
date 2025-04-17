b59a64f9dc452aa87986ee71c9a33ac08199018d
Art San <artsan.mcc@gmail.com>
2025-04-17 13:13:49 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,39 @@ import { getInitialTheme } from './helpers/getInitialTheme'
 import { toggleTheme } from './helpers/toggleTheme'
 
 function App() {
-  const initialTodos = [
-    { id: 1, text: 'Изучить React' },
-    { id: 2, text: 'Сделать TODO app' },
-    { id: 3, text: 'Сделать деплой' }
-  ]
-  const [todos, setTodos] = useState(initialTodos)
+  const [todos, setTodos] = useState([])
   const [theme, setTheme] = useState(getInitialTheme())
 
-  const onAdd = (text) => {
+  const onAdd = (text, deadline) => {
     const newTodo = {
       id: Date.now(),
-      text
+      text,
+      completed: false,
+      createdAt: new Date().toISOString(),
+      deadline: deadline || null,
+      order: todos.length + 1
     }
     setTodos([...todos, newTodo])
+  }
+
+  const toggleComplete = (id) => {
+    console.log(123, id)
+    const todoToUpdate = todos.find((todo) => todo.id === id)
+
+    if (!todoToUpdate) return
+
+    const updatedTodo = {
+      ...todoToUpdate,
+      completed: !todoToUpdate.completed
+    }
+
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? updatedTodo : todo
+    )
+
+    console.log(125, updatedTodos)
+
+    setTodos(updatedTodos)
   }
 
   const onDelete = (id) => {
@@ -41,7 +60,12 @@ function App() {
         <AddTodo onAdd={onAdd} />
         <div className="flex flex-col gap-3">
           {todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} onDelete={onDelete} />
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={onDelete}
+              onToggleComplete={toggleComplete}
+            />
           ))}
         </div>
       </div>
